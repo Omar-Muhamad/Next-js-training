111e2285aa21def65ac9290db515658af1d98a83
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2023-02-27 22:27:44 +0200

    Add posts dynamic routes

--- src/app/blog/[...slug]/page.jsx
+++ src/app/blog/[...slug]/page.jsx
@@ -1,5 +1,22 @@
-const Post = () => {
-  return <div>Post</div>;
+const getData = async (slug) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${slug}`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const Post = async ({ params }) => {
+  const { slug } = params;
+  const post = await getData(slug);
+  return (
+    <main className="p-5 m-5 rounded-xl bg-white">
+      <div className="text-black">
+        <h3 className="font-bold text-lg mb-5">{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    </main>
+  );
 };
 
 export default Post;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,41 @@
-const Blog = () => {
-  return <div>Blog</div>;
+import Link from 'next/link';
+
+const getData = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+  <main className='flex flex-col justify-center items-center gap-10'>
+    <h1 className='font-bold text-3xl'>Blog Posts</h1>
+    <ul className='grid grid-cols-4 p-10'>
+      {data.map((post, index) => (
+        <li 
+        className='p-5 m-5 rounded-xl bg-white'
+        key={index}>
+          <Link href={`blog/${post.id}`}>
+            <div className="text-black">  
+              <h3 className='font-bold text-lg mb-5'>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </main>
+  );
+};
+
+export const getStaticParams = async () => {
+  const data = await getData();
+  let params = [];
+  for (id of data) {
+    params.push({ slug: id });
+  }
+  return params;
 };
 
 export default Blog;
