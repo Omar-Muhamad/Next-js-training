7ad9fb0f7f4a25523fc6e6e4db170e5b2d7538af
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2023-03-01 12:07:23 +0200

    Change home page

--- src/app/blog/[...slug]/page.jsx
+++ src/app/blog/[...slug]/page.jsx
@@ -10,10 +10,13 @@ const Post = async ({ params }) => {
   const { slug } = params;
   const post = await getData(slug);
   return (
-    <main className="p-5 m-5 rounded-xl bg-white">
-      <div className="text-black">
-        <h3 className="font-bold text-lg mb-5">{post.title}</h3>
-        <p>{post.body}</p>
+    <main className="flex flex-col justify-center items-center gap-10">
+      <h1 className="font-bold text-3xl">Blog Post</h1>
+      <div className="p-5 m-5 rounded-xl bg-white">
+        <div className="text-black">
+          <h3 className="font-bold text-lg mb-5">{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
       </div>
     </main>
   );
